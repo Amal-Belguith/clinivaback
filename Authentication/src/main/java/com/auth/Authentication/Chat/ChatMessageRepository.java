96d2f635b5b8aa792090049eaf8c689302709e89
Amal Belguith <amal.belghith@iss-4u.com>
2024-06-26 14:19:45 +0100

    second commit

--- Authentication/src/main/java/com/auth/Authentication/Chat/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-/*package com.auth.Authentication.Chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage,String> {
-    List<ChatMessage> findByChatId(String s);
-}*/
--- Authentication/src/main/java/com/auth/Authentication/ChatRoom/ChatRoomRepo.java
+++
@@ -1,9 +0,0 @@
-/*package com.auth.Authentication.ChatRoom;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ChatRoomRepo extends JpaRepository<ChatRoom,String> {
-    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
-}*/
